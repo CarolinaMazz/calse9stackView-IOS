3da2c274685692dbb84bc4cca146f4a264d8586b
alumno <alumno@a51-03.local>
2017-02-16 19:11:10 -0500

    Initial Commit

---
+++ calse 9 stackView IOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  calse 9 stackView IOS
+//
+//  Created by alumno on 16/02/17.
+//  Copyright © 2017 william. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
